1afa0107cbd119b5a2e4ee188a7b8c86d3064770
Itamarhf <136043045+Itamarhf@users.noreply.github.com>
2023-06-16 23:29:22 -0500

    Add files via upload

---
+++ itamar12-soap-J19/src/main/java/pe/edu/idat/dsw1soapJ19/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package pe.edu.idat.dsw1soapJ19;
+
+import io.spring.guides.gs_producing_web_service.GetDayReq;
+import io.spring.guides.gs_producing_web_service.GetDayRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getDayReq")
+	@ResponsePayload
+	public GetDayRes getCountry(@RequestPayload GetDayReq request) {
+		var response = new GetDayRes();
+		response.setTranslation(countryRepository.findCountry(request.getName()));
+		//System.out.println(response.getTranslation());
+		return response;
+	}
+}
---
+++ itamar12-soap-J19/src/main/java/pe/edu/idat/dsw1soapJ19/CountryRepository.java
@@ -0,0 +1,51 @@
+package pe.edu.idat.dsw1soapJ19;
+
+import io.spring.guides.gs_producing_web_service.Day;
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class CountryRepository {
+    
+    private static final Map<String, Day> countries = new HashMap<>();
+
+	@PostConstruct
+	public void initData() {
+		var lunes = new Day();
+		lunes.setName("Monday");
+		countries.put("lunes", lunes);
+		var martes = new Day();
+		martes.setName("Tuesday");
+		countries.put("martes", martes);
+		var miercoles = new Day();
+		miercoles.setName("Wednesday");
+		countries.put("miercoles", miercoles);
+
+		var jueves = new Day();
+		jueves.setName("Thursday");
+		countries.put("jueves", jueves);
+
+		var viernes = new Day();
+		viernes.setName("Friday");
+		countries.put("viernes", viernes);
+
+		var sabado = new Day();
+		sabado.setName("Saturday");
+		countries.put("sabado", sabado);
+
+		var domingo = new Day();
+		domingo.setName("Sunday");
+		countries.put("domingo", domingo);
+	}
+
+
+	public Day findCountry(String name) {
+		Assert.notNull(name, "The country's name must not be null");
+		//System.out.println(countries.get("Monday").getName());
+		return countries.get(name);
+	}
+}
