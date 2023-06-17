4e56d63a91bd37228a0c4ee41e050b51f50cc5ee
Itamarhf <136043045+Itamarhf@users.noreply.github.com>
2023-06-16 23:35:53 -0500

    Update WebServiceConfig.java

--- itamar12-soap-J19/src/main/java/pe/edu/idat/dsw1soapJ19/WebServiceConfig.java
+++ itamar12-soap-J19/src/main/java/pe/edu/idat/dsw1soapJ19/WebServiceConfig.java
@@ -1,4 +1,4 @@
-package pe.edu.idat.dsw1soapJ19;
+package pe.edu.idat.itamar12soapJ19;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
